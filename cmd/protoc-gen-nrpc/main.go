8b6360a93c4be573995d9e5c7f1afcb2d9b85c90
瑞雯 <liuwch@yoozoo.com>
2021-08-03 13:15:08 +0800

    [update] natsrpc

--- cmd/protoc-gen-nrpc/main.go
+++ cmd/protoc-gen-nrpc/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/byebyebruce/natsrpc/cmd/protoc-gen-nrpc/generator"
 	pgs "github.com/lyft/protoc-gen-star"
+	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
 )
 
 func main() {
@@ -15,5 +16,6 @@ func main() {
 	//	Render()
 	pgs.Init(pgs.DebugEnv("DEBUG")).
 		RegisterModule(generator.New()).
+		RegisterPostProcessor(pgsgo.GoFmt()).
 		Render()
 }
