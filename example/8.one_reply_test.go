cec885357ac9cf616c3b0d23ee4de4eeba0392e9
lu.bai <lu.bai1@funplus.com>
2022-07-08 12:07:27 +0800

    add 单回复示例

---
+++ example/8.one_reply_test.go
@@ -0,0 +1,60 @@
+package example
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/byebyebruce/natsrpc"
+	"github.com/byebyebruce/natsrpc/example/pb/request"
+	"github.com/byebyebruce/natsrpc/testdata"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var handleCnt int32 = 0
+
+type OneSvc struct {
+	id int
+}
+
+func (h *OneSvc) Hello(ctx context.Context, req *testdata.HelloRequest) (*testdata.HelloReply, error) {
+	atomic.AddInt32(&handleCnt, 1)
+	// 只有1号svc回复
+	if h.id != 1 {
+		return nil, nil
+	}
+	return &testdata.HelloReply{
+		Message: fmt.Sprint(h.id),
+	}, nil
+}
+
+func (h *OneSvc) HelloError(ctx context.Context, req *testdata.HelloRequest) (*testdata.HelloReply, error) {
+	return nil, fmt.Errorf(haha)
+}
+
+func TestOneReply(t *testing.T) {
+	cms1 := &OneSvc{id: 1}
+	svc, err := request.RegisterGreeterNATSRPCServer(server, cms1, natsrpc.WithServiceNoGroup())
+	assert.Nil(t, err)
+	defer svc.Close()
+
+	cms2 := &OneSvc{id: 2}
+	svc, err = request.RegisterGreeterNATSRPCServer(server, cms2, natsrpc.WithServiceNoGroup())
+	assert.Nil(t, err)
+	defer svc.Close()
+
+	cli, err := request.NewGreeterNATSRPCClient(enc)
+	assert.Nil(t, err)
+
+	resp, err := cli.Hello(context.Background(), &testdata.HelloRequest{
+		Name: haha,
+	})
+	time.Sleep(time.Millisecond * 10)
+	assert.Nil(t, err)
+	assert.Equal(t, "1", resp.Message)
+	assert.EqualValues(t, 2, handleCnt)
+
+}
